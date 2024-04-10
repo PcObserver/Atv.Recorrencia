062532ef53857de320725550ad8bdd0d96efd95b
lucas <lucas.gbastos@hotmail.com>
2024-04-10 20:30:42 -0300

    refactored

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,25 @@ public class Main {
         System.out.println("Hello world!");
         ScriptEngineManager sem = new ScriptEngineManager();
         ScriptEngine se = sem.getEngineByName("JavaScript");
-        String equiation = "2^2";
-        Object result = resolveEquation(equiation, 2, 2);
+        String equiation = "T(n-1)^n";
+        Object result = calculateRecurrenceSequencePositionValue(equiation, 3, 2);
         System.out.println("Result = "+result);
     }
 
-    private static Object resolveEquation(String equation, int n, int n1Value){
+    private static Object calculateRecurrenceSequencePositionValue(String equation, int n, int n1Value){
         if(n==1) return n1Value;
-        String processedEquation = equation.replace("T(n-1)", String.valueOf(resolveEquation(equation,
+        String processedEquation = equation.replace("T(n-1)", String.valueOf(calculateRecurrenceSequencePositionValue(equation,
                 n-1,n1Value)));
-        System.out.println(processedEquation);
+        processedEquation = processedEquation.replace("n", String.valueOf(n));
         processedEquation = replacePowersWithMathPow(processedEquation);
-        System.out.println(processedEquation);
+        return resolveEquation(processedEquation);
+    }
+
+    private static Object resolveEquation(String processedEquation) {
         ScriptEngineManager sem = new ScriptEngineManager();
         ScriptEngine se = sem.getEngineByName("JavaScript");
         try {
             Object r =  se.eval(processedEquation);
-            System.out.println(r);
             return r;
 
         }catch (Exception e){
@@ -30,10 +32,12 @@ public class Main {
             return -1;
         }
     }
+
     private static String replacePowersWithMathPow(String equation) {
-        String regex = "(\\d+(\\.\\d+)?|\\([^(^)]+\\))\\^(\\d+(\\.\\d+)?|\\.\\d+|\\([^(^)]+\\))";
-        String replacement = "Math.pow($1, $3)";
-        while (equation.matches("." + regex + "."))
+        String regex = "(\\d+(\\.\\d+)?|\\.\\d+|\\([^(^)]+\\))\\^(\\d+(\\.\\d+)?|\\.\\d+|\\([^(^)]+\\))";
+
+        String replacement = "Math.pow($1,$3)";
+        while (equation.contains("^"))
 
         {
             equation = equation.replaceAll(regex, replacement);
